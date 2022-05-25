fd98ce181a50f0a620b6160b885bd92f9e770591
Mateo Cevallos <66786471+matojo074@users.noreply.github.com>
2022-05-25 21:20:50 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+
+      echo '<p>Hello World</p>';
+      //Todas las variables deben de comenzar con el signo de $
+      $nombre = "Mateo";
+      $apellido = "Cevallos";
+
+echo "Hola ".$nombre. ' '.$apellido;
+echo "<hr>";
+echo "Hola, mi nombre es: $nombre"
+    ?> 
+  </body>
+</html>
